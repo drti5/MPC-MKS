38b7fe056eec462113478c8a00f71f5551d2fa62
drti5 <vojta.drtina@seznam.cz>
2024-10-16 10:00:35 +0200

    cv04

---
+++ cv04/cv04/Core/Inc/sct.h
@@ -0,0 +1,18 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 9, 2024
+ *      Author: 240605
+ */
+
+#ifndef SRC_SCT_H_
+#define SRC_SCT_H_
+
+void sct_init(void);
+
+void sct_value(uint16_t value, uint8_t led);
+
+void sct_led(uint32_t value);
+
+
+#endif /* SRC_SCT_H_ */
